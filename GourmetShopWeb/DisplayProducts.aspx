89de575fb9b71217b07f45820c43c16a31cdcab8
DannyGuerreroo <dannyguerrerostudent@gmail.com>
2023-04-24 22:43:22 -0700

    Created Display Products page that displays products with a search bar for names

---
+++ GourmetShopWeb/DisplayProducts.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="DisplayProducts.aspx.cs" Inherits="GourmetShopWeb.DisplayProducts" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    Search Item Names: <asp:TextBox ID="searchBarText" runat="server"></asp:TextBox>
+    <asp:Button ID="searchBarButton" runat="server" Text="Search" OnClick="searchBarButton_Click1" />
+    <asp:GridView ID ="listProductsGrid" runat="server" ShowHeaderWhenEmpty="true" EmptyDataText="No Items Found">
+    </asp:GridView>
+</asp:Content>
---
+++ GourmetShopWeb/DisplayProducts.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace GourmetShopWeb
+{
+	public partial class DisplayProducts : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			BindData();
+		}
+
+		private void BindData()
+		{
+			using (SqlConnection conn = new SqlConnection(db.getConnectionString()))
+			{
+				using (SqlCommand cmd = new SqlCommand("SELECT * FROM product WHERE ProductName like '%'+@Name+'%'", conn))
+				{
+					cmd.Parameters.AddWithValue("Name", searchBarText.Text);
+					conn.Open();
+					using (SqlDataReader dr = cmd.ExecuteReader())
+					{
+						listProductsGrid.DataSource = dr;
+						listProductsGrid.DataBind();
+					}
+				}
+			}
+		}
+
+
+		protected void searchBarButton_Click1(object sender, EventArgs e)
+		{
+			BindData();
+		}
+	}
+}
